48e8f005975c4c58ab10637bb77536f0db2fccfc
petelouise <programmetress@petelouise.beauty>
2025-01-30 21:39:33 -0600

    refactor, goodbye to hello

--- src/midbuddy/cli.py
+++ src/midbuddy/cli.py
@@ -1,29 +1,40 @@
+import os
 import re
 import webbrowser
 
 import click
 import exiftool
-
-from .hello import process_file
+import pyexiv2
 
 
 @click.group()
-def cli():
-    """midbuddy - helps with midjourney."""
+def cli() -> None:
+    """
+    midbuddy - helps with midjourney.
+    """
     pass
 
 
 @cli.group()
 def meta() -> None:
-    """metadata management commands."""
+    """
+    metadata management commands.
+    """
     pass
 
 
 @meta.command()
 @click.argument("path", type=click.Path(exists=True))
 def add(path) -> None:
-    """add midjourney metadata to an image file."""
-    url = process_file(path)
+    """
+    add midjourney metadata to an image file.
+    """
+    url = get_url(path)
+    with pyexiv2.Image(path) as img:
+        # Set IPTC Source metadata
+        img.modify_iptc({"Iptc.Application2.Source": url})
+        # Set XMP URL metadata
+        img.modify_xmp({"Xmp.xmp.Identifier": url})
     click.echo(f"added metadata url: {url}")
 
 
@@ -33,43 +44,41 @@ def open(path) -> None:
     """
     opens the midjourney job in a web browser.
     """
-    try:
-        with exiftool.ExifToolHelper() as et:
-            metadata = et.get_metadata(path)[0]
-            click.echo(f"Full metadata:\n{metadata}")
+    url = get_url(path)
+    click.echo(f"Opening URL: {url}")
+    webbrowser.open(url)
 
-        description = (
-            metadata.get("PNG:Description")
-            or metadata.get("XMP:Description")
-            or metadata.get("EXIF:ImageDescription")
-        )
-        click.echo(f"XMP:Description: {metadata.get('XMP:Description')}")
-        click.echo(f"EXIF:ImageDescription: {metadata.get('EXIF:ImageDescription')}")
 
-        if not description:
-            raise ValueError("No description metadata found in the image.")
+def get_url(path: str) -> str:
+    with exiftool.ExifToolHelper() as et:
+        metadata = et.get_metadata(path)[0]
 
-        job_id_prefix = "Job ID: "
-        if job_id_prefix not in description:
-            raise ValueError(f"No 'Job ID' field found in description: {description}")
+    description = (
+        metadata.get("PNG:Description")
+        or metadata.get("XMP:Description")
+        or metadata.get("EXIF:ImageDescription")
+    )
 
+    job_id = None
+    job_id_prefix = "Job ID: "
+
+    if description and job_id_prefix in description:
+        click.echo("found job id prefix in description metadata")
         job_id = description.split(job_id_prefix)[-1].strip()
-        if not job_id:
-            raise ValueError("The 'Job ID' field is empty or invalid.")
 
-        index_pattern = r"_([0-3])(?=\.[^\.]+$)"
-        match = re.search(index_pattern, path)
-        index = match[1] if match else None
+    name, _ = os.path.splitext(path)
+    if not job_id:
+        job_id = name[-38:-2]
+    index = int(name[-1])
+    if not job_id:
+        raise ValueError("could not find job id in metadata or file path")
 
-        url = f"https://www.midjourney.com/jobs/{job_id}"
+    url = f"https://www.midjourney.com/jobs/{job_id}"
 
-        if index:
-            url += f"?index={index}"
+    if index:
+        url += f"?index={index}"
 
-        click.echo(f"Opening URL: {url}")
-        webbrowser.open(url)
-    except Exception as e:
-        click.echo(f"Unexpected Error: {e}", err=True)
+    return url
 
 
 if __name__ == "__main__":
